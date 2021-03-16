5662b37a0c9b1231e95a0dd0a09b155ca3b222a9
voldemort <alabenothman@gmail.com>
2021-03-16 14:37:12 -0700

    Created few methods

--- chess_1/modules/slideable.rb
+++ chess_1/modules/slideable.rb
@@ -7,4 +7,24 @@ module Slideable
         
     end
 
+    def moves 
+        #assumed #move_dirs is an array with directions 
+        moves = []
+        curr_row,curr_col = @pos
+        if move_dirs.include?(:horizontal) 
+             
+            (0..7).each do |new_col|
+                moves << [curr_row,new_col] unless new_col == curr_col 
+            end
+        end
+        if move_dirs.include?(:vertical)
+            (0..7).each do |new_row|
+                moves << [new_row,curr_col] unless new_row == curr_row
+            end
+        end
+        if move_dirs.include?(:diagonal)
+            
+        end
+    end
+
 end
