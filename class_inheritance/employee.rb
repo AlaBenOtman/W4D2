c23859ca7910cd9afb73d5e59a8191d857179eb3
Chris Joo <chrisj1225@gmail.com>
2021-03-16 15:21:03 -0400

    Fix bonus method

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -1,21 +1,22 @@
 class Employee #superclass / parent class
 
+    attr_reader :name, :title, :salary, :boss
     def initialize(name, title, salary, boss=nil)
         @name = name
         @title = title
         @salary = salary
-        @boss = boss
+        @boss = boss #Manager ivar
+        set_employees
     end
 
     def bonus(multiplier)
         bonus = @salary * multiplier
     end
 
-    def boss=(manager) #######
-        if !@boss.nil?
+    def set_employees
+        unless @boss.nil?
             @boss.employees << self
         end
-
     end
 
 end
--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -1,34 +1,41 @@
+require 'byebug'
 require_relative 'employee'
 #sub class / child class
 class Manager < Employee 
 
-    def initialize(name, title, salary, boss, employees)
-        super(name, title, salary, boss)
+    attr_reader :employees
+    def initialize(name, title, salary, boss=nil)
+        super#(name, title, salary, boss)
         @employees = [] #employees arg is an array of employee instances
     end
 
     def bonus(multiplier)
         total_salary = 0
-
+        # need to fix loop... multiplying total_salary at every recursive call...
+        # method should only multiply once at the end.
         @employees.each do |employee|
-            if employee.title == "Founder" || employee.title == "TA Manager"
+            # debugger
+            if employee.title == "Founder"
                 total_salary += employee.bonus(multiplier)
+            elsif employee.title == "TA Manager"
+                total_salary += employee.bonus(multiplier) 
             else 
                 total_salary += employee.salary
             end
         end
+        total_salary * multiplier
     end
-
-     
-
 end
 
 #Ned --> Darren --> Shawna & David
 
-shawna = Employee.new("shawna", "TA", 12000, "darren")
-david = Employee.new("shawna", "TA", 10000, "darren")
+# employees = [shawna, david]
+ned = Manager.new("ned", "Founder", 1000000, nil)
+darren = Manager.new("darren", "TA Manager", 78000, ned)
+shawna = Employee.new("shawna", "TA", 12000, darren)
+david = Employee.new("shawna", "TA", 10000, darren)
 
-employees = [shawna, david]
-darren = Manager.new("darren", "TA Manager", "ned", employees)
 
-
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
